b42c8b61bc4bb6745a983b2ea7d4828faf137baa
ben <futureappworker@gmail.com>
2024-01-16 22:27:42 +0800

    feat: add basic emails

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(express.json());
+
+app.post('/emails', (req, res) => {
+  const {
+    to,
+    subject,
+    body,
+  } = req.body;
+  if (!to || !subject || !body) {
+    return res.status(400).json({
+      message: '缺少參數'
+    });
+  }
+  res.json({
+    message: '成功'
+  });
 })
 
 app.listen(PORT, () => {
