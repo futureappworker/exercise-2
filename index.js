a6ef711a681a609e5a1f29fbf4e57270e04772da
ben <futureappworker@gmail.com>
2024-01-16 22:40:41 +0800

    feat: add nodemailer and send mail

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS,
+  }
+});
 
 app.post('/emails', (req, res) => {
   const {
@@ -15,8 +24,21 @@ app.post('/emails', (req, res) => {
       message: '缺少參數'
     });
   }
-  res.json({
-    message: '成功'
+  const data = {
+    from: 'exercise-2 <exercise-2@example.com>',
+    to,
+    subject,
+    text: body,
+  };
+  transporter.sendMail(data, (err) => {
+    if (err) {
+      return res.status(500).json({
+        message: '伺服器錯誤'
+      });
+    }
+    return res.json({
+      message: '成功'
+    });
   });
 })
 
